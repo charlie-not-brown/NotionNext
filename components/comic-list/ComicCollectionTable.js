01f49dddfdad020415c25d3db6dfb355c152de49
charlie-not-brown <charlie_not_brown@protonmail.com>
2026-07-17 01:57:54 +0800

    排查问题

--- components/comic-list/ComicCollectionTable.js
+++ components/comic-list/ComicCollectionTable.js
@@ -4,21 +4,12 @@ const ComicCollectionTable = ({
   ctx,
   ...collectionProps
 }) => {
-  /*
-   * 目前只验证目标数据库是否能
-   * 正确进入这个组件。
-   *
-   * 暂时仍使用 react-notion-x
-   * 原版 Collection 渲染数据库。
-   */
   return (
-    <div data-comic-collection='true'>
-      <OriginalCollection
-        block={block}
-        ctx={ctx}
-        {...collectionProps}
-      />
-    </div>
+    <OriginalCollection
+      block={block}
+      ctx={ctx}
+      {...collectionProps}
+    />
   )
 }
 
